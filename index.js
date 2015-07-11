6942771c74f4f6a8af7d1cf22ee867d3a13afd7b
medvedev-dmitriy <medvedev-dmitriy@rambler.ru>
2015-07-11 20:43:00 +0300

    work whithout generate errors

--- index.js
+++ index.js
@@ -3,15 +3,39 @@ var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
 
-faker.locale = 'uk';
-var randomName = faker.name.findName();
-var phoneNumber = faker.phone.phoneNumber();
-var city = faker.address.city();
-var street = faker.address.streetAddress();
-console.log(
-    randomName + " ; " +
-    city + " " +
-    street + " ; " +
-    phoneNumber + " "
+if(process.argv[2] == 'EN'){
+    faker.locale = 'en';
+} else if(process.argv[2] == 'RU'){
+    faker.locale = 'ru';
+} else if (process.argv[2] == 'BL'){
+    faker.locale = 'uk';
+} else {
+    console.log("invalid params");
+    return;
+}
 
-);
+if (!isNaN(process.argv[3]) && !isNaN(process.argv[4])){
+    var n = process.argv[3];
+    var errors = process.argv[4];
+} else {
+    console.log("invalid params");
+    return;
+}
+
+for (var i = 0; i < n; i++) {
+    var randomName = faker.name.findName();
+    var city = faker.address.city();
+    var street = faker.address.streetAddress();
+    var phoneNumber = faker.phone.phoneNumber();
+    show(randomName, city, street, phoneNumber);
+}
+
+
+function show(name, city, street, phone){
+    console.log(
+        name + " ; " +
+        city + " " +
+        street + " ; " +
+        phone + " "
+    );
+}
